8ad703cf4cb52d92f7221ea31fe1febd6ce3c5cc
sergioperez <sermanaha@gmail.com>
2025-06-15 15:54:24 -0500

    COMMIT 4 : CONFIGURACION DE VISTAS

---
+++ src/main/java/com/udc/edificios3/Edificios3Application.java
@@ -0,0 +1,13 @@
+package com.udc.edificios3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edificios3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Edificios3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/udc/edificios3/Edificios3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udc.edificios3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Edificios3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
